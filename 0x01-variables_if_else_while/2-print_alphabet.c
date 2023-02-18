60985a7dddd900e2209e89f65a345de9affd7fa2
Nickson Rono <kiprotichnickson20@gmail.com>
2023-02-17 18:32:04 -0800

    description

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,23 +1,24 @@
 #include <stdio.h>
 
 /**
- * main - alphabet in lowercase, followed by a new line.
+ * main - Entry point
  *
- * Always return(0)
- */
+ * Description: print all aplhabet letters
+ *
+ * Return: Always 0 (Success)
+*/
 
 int main(void)
 {
 	char ch = 'a';
 
-	for (ch = 'a'; ch <= 'z'; ch++)
+	while (ch <= 'z')
 	{
 		putchar(ch);
+		++ch;
 	}
 	putchar('\n');
 
-	/**
-	 * Return value of main
-	 *
-	 * */
+	return (0);
 }
+
