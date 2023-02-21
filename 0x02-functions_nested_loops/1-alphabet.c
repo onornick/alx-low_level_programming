f50bff1eec919dedc5e87fda1bb97b32a7505365
Nickson Rono <kiprotichnickson20@gmail.com>
2023-02-21 12:42:42 -0800

    initial commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/*
+ * main - check the code
+ *
+ * Return: Always 0
+ */
+int main()
+{
+print_alphabet(void)
+{
+int i;
+for(i=65; i<=90; i++)
+{
+_putchar(i);
+}
+}
+return (0);
+}
