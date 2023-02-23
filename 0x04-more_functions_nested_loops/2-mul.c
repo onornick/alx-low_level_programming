e50c80220fce4d99a4dbb6f82d79a3b6304add8b
Nickson Rono <kiprotichnickson20@gmail.com>
2023-02-23 13:33:52 -0800

    betty style

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -3,12 +3,14 @@
  * mul - multiplies two integers.
  *
  * @a: first parameter
- * @b 2nd paramenter
+ * @b: 2nd paramenter
+ *
+ * Return: Always 0;
  *
  */
 
 int mul(int a, int b)
 {
-	return (a*b);
+	return (a * b);
 
 }
