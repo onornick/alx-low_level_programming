b58bc61361c47b18885f4a1b1607ebd9a52150be
Nickson Rono <kiprotichnickson20@gmail.com>
2023-02-23 15:35:29 -0800

    add declaration for i

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_most_numbers - prints the numbers, from 0 to 9, followed by a new line.
+ * print_most_numbers - prints the numbers, from 0 to 9.
  *
  * Return: Always 0
  *
@@ -8,9 +8,11 @@
 
 void print_most_numbers(void)
 {
+	int i;
+
 	for (i = 0; i <= 9; i++)
 	{
-		if (i == 2 || i ==4)
+		if (i == 2 || i == 4)
 			continue;
 		else
 			_putchar(i);
