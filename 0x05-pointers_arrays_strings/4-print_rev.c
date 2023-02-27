728bede4424416df52568d71ef1645b06e70123b
Nickson Rono <kiprotichnickson20@gmail.com>
2023-02-27 15:01:16 -0800

    initial commit

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_rev - rints a string, in reverse, followed by a new line.
+ *
+ * @s: parameter
+ *
+ * Return: Always 0.
+ */
+
+void print_rev(char *s)
+{
+	int len = 0;
+
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+	for (int i = len - 1; i >= 0; i--)
+	{
+	_putchar(str[i]);
+	}
+	_putchar('\n');
+}
