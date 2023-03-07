301340b980c8fb768f18ae6308b8a53f081605c7
Nickson Rono <kiprotichnickson20@gmail.com>
2023-03-08 01:48:52 +0300

    Betty style

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,14 +1,14 @@
-#include "main.h"
+#include"main.h"
 
 /**
- * print_chessboard - prints the chessboard
- * @a: two dimension array to print
- */
+*print_chessboard-printsthechessboard
+*@a:twodimensionarraytoprint
+*/
 
-void print_chessboard(char (*a)[8])
+void print_chessboard(char(*a)[8])
 {
 	int i, j;
-	
+
 	for (i = 0; i < 8; i++)
 	{
 		for (j = 0; j < 8; j++)
