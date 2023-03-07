e671e18b9622b2780b473ab4e6457368662ebdd4
Nickson Rono <kiprotichnickson20@gmail.com>
2023-03-07 23:33:24 +0300

    2nd commit

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,26 +1,25 @@
-#include "main.h"
 /**
- * _memcpy - copies memory area.
- * @src: parameter
- * @dest: parameter
- * @n: parameter
+ * _strchr - Locates a character in a string.
+ * @s: The string to search.
+ * @c: The character to locate.
  *
- * Return: a pointer to dest
+ * Return: If the character is found, return a pointer to the first occurrence
+ *         of the character in the string. Otherwise, return NULL.
  */
 char *_strchr(char *s, char c)
 {
-		int a;
-
-		while (1)
+	while (*s != '\0')
+	{
+		if (*s == c)
 		{
-			a = *s++;
-			if (a == c)
-			{
-				return (s - 1);
-			}
-			if (a == 0)
-			{
-				return (NULL);
-			}
+			return (s);
 		}
+		s++;
+	}
+	if (c == '\0')
+	{
+		return (s);
+	}
+	return (NULL);
 }
+
