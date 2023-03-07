311968facb8c28c67555b3365b1467576143a63f
Nickson Rono <kiprotichnickson20@gmail.com>
2023-03-07 15:24:48 +0300

    latest commit

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -7,14 +7,15 @@
  *
  * Return: a pointer to dest
  */
+
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
 
-	while (i < n)
+	for (i = 0; i < n; i++)
 	{
 		dest[i] = src[i];
-		i++;
 	}
+
 	return (dest);
 }
