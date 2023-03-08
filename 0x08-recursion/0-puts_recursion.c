caebeffd0f8b997e1fa3b59b1783dc6a9ad1087d
Nickson Rono <kiprotichnickson20@gmail.com>
2023-03-09 01:08:03 +0300

    Betty Style

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,6 +1,5 @@
 #include "main.h"
 /**
- *
  * _puts_recursion - prints a string, followed by a new line.
  * @s: parameter
  *
@@ -14,7 +13,7 @@ void _puts_recursion(char *s)
 	else
 	{
 		_putchar(*s);
-		_puts_recursion(s+1);
+		_puts_recursion(s + 1);
 	}
 
 }
