ce0cfeb374f7b042612764b52084b8197c7486fa
Nickson Rono <kiprotichnickson20@gmail.com>
2023-03-13 13:41:09 +0300

    initial commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * main - prints the name of the program
+ * @argc - count of cmd arguments
+ * @argv - array of strings in cmd arguments
+ *
+ */
+int main(int argc, char *argv[])
+{
+	_putchar(argv[0]);
+}
