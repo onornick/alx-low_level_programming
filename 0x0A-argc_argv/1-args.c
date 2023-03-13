6680067c0f0b56f2aedf8ec1c512131c97926964
Nickson Rono <kiprotichnickson20@gmail.com>
2023-03-13 22:54:22 +0300

    initial commit

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints the number of cmd arguments
+ * @argc: cmd count
+ * @argv: argument vectors
+ * Return: Always 0.
+ *
+ *
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argv;
+
+	if (argc > 0)
+	{
+		printf("%d", argc);
+		printf("\n");
+	}
+	return (0);
+}
