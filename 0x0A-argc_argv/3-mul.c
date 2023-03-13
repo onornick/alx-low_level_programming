89f3e8723f5985901744c93fa4d6986a808bc3f0
Nickson Rono <kiprotichnickson20@gmail.com>
2023-03-14 02:20:11 +0300

    initial commit

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - muliplies 2 numbers
+ * @argc: count
+ * @argv: vector
+ * Return: Always 0.
+ *
+ *
+ */
+
+int main(int argc, char *argv[])
+{
+	int result;
+
+	if (argc < 3)
+	{
+		printf("%s\n", "Error");
+		return (1);
+	}
+
+	else
+	{
+		int num1 = atoi(argv[1]);
+
+		int num2 = atoi(argv[2]);
+
+		result = num1  * num2;
+		printf("%d\n", result);
+	}
+
+	return (0);
+
+}
