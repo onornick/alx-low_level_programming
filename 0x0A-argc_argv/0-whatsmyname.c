bb46fa15b83244c5d79ad210e2f6442f3aeac867
Nickson Rono <kiprotichnickson20@gmail.com>
2023-03-13 14:46:52 +0300

    initial commit

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,12 +1,21 @@
 #include "main.h"
-
 /**
  * main - prints the name of the program
  * @argc - count of cmd arguments
  * @argv - array of strings in cmd arguments
  *
  */
+
 int main(int argc, char *argv[])
 {
-	_putchar(argv[0]);
+	char *str = argv[0];
+	int i;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+	return (0);
 }
