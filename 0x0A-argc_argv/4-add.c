262fe44f8a417f38f2b8978ceeac6b4eeaadac4f
Nickson Rono <kiprotichnickson20@gmail.com>
2023-03-14 14:15:07 +0300

    initial commit

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * main - adds up cmd arguments
+ * @argc: argument count
+ * @argv: argument vectors
+ * Return: Always 0;
+ *
+ *
+ */
+
+int main(int argc, char *argv[])
+{
+	int result;
+	int i, j;
+
+	if (argc <= 2)
+	{
+		printf("%d\n", 0);
+	}
+
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (!isdigit(argv[i][j]))
+			{
+				printf("%s\n", "Error");
+				return (1);
+			}
+
+		}
+		result += atoi(argv[i]);
+
+
+
+	}
+	printf("%d", result);
+	return (0);
+}
