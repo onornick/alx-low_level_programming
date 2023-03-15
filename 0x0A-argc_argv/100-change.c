f6c6ed4e01e32d034bee48db03f00ddd67faca21
Nickson Rono <kiprotichnickson20@gmail.com>
2023-03-15 11:21:24 +0300

    initial commit

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: 0 if success, 1 if error
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+	int cents, num_coins = 0;
+	int coins[5] = {25, 10, 5, 2, 1};
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	cents = atoi(argv[1]);
+	if (cents < 0)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	for (i = 0; i < 5; i++)
+	{
+		num_coins += cents / coins[i];
+		cents %= coins[i];
+	}
+
+	printf("%d\n", num_coins);
+	return (0);
+}
+
