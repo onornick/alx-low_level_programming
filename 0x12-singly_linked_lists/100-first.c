bc38e9fbfa2590075a155e58cdf359eda592c84d
Nick <kiprotichnickson20@gmail.com>
2023-04-01 16:31:28 +0300

    removed space after \n

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -10,6 +10,6 @@ void b4_main(void) __attribute__((constructor));
 void b4_main(void)
 {
 
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n ");
+	printf("%s","You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
 
 }
