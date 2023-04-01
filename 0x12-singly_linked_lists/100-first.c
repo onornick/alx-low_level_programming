20974b56d4a328779a15e6796da28c6e690a263a
Nick <kiprotichnickson20@gmail.com>
2023-04-01 16:24:38 +0300

    initial commit

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+
+/**
+ * b4_main - Constructor function that executes before main()
+ *
+ * Description: This function is executed before the main() function is called.
+ * It prints a message to the console.
+ */
+void b4_main(void) __attribute__((constructor));
+
+void b4_main(void)
+{
+
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n ");
+
+}
