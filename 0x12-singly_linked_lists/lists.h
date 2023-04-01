f99691b15242153a47bdbb5f0a177741201d2271
Nick <kiprotichnickson20@gmail.com>
2023-04-01 06:00:07 +0300

    removed typedef

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -16,11 +16,5 @@ void free_list(list_t *head);
  *
  * Description: singly linked list node structure
  */
-typedef struct list_s
-{
-    char *str;
-    unsigned int len;
-    struct list_s *next;
-} list_t;
 
 #endif
