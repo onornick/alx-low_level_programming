d11575a94209d8ebf17b9f96129b7df140f5f180
Nick <kiprotichnickson20@gmail.com>
2023-04-05 02:32:32 +0300

    free dynamically allocated memory

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -12,6 +12,9 @@ void free_listint2(listint_t **head)
 {
 	listint_t *currentNode;
 
+	if (head == NULL)
+		return;
+
 	while (*head != NULL)
 	{
 		currentNode = *head;
@@ -19,5 +22,4 @@ void free_listint2(listint_t **head)
 		free(currentNode);
 	}
 
-	*head = NULL;
 }
