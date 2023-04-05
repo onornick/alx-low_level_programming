2bc0957685ac7b83c951e1d22422bf29a7f594e2
Nick <kiprotichnickson20@gmail.com>
2023-04-05 16:49:40 +0300

    initial commit

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ *
+ */
+
+int sum_listint(listint_t *head)
+{
+	int sum = 0;
+
+	if (head == NULL)
+		return (0);
+
+	while (head != NULL)
+	{
+		sum = head->n;
+		head = head->next;
+	}
+
+
+	return (sum);
+
+}
+
+
