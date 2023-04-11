4f5427dee715c1d1350073b46d4a519959da03c5
Nick <kiprotichnickson20@gmail.com>
2023-04-11 23:56:56 +0300

    bug fix

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -5,30 +5,29 @@
  * @n: The unsigned long integer to print
  * Return: nothing
  */
+
 void print_binary(unsigned long int n)
 {
-	unsigned long int bit_mask;
+	unsigned long int copy_of_n = n, bit_mask = 1;
+	int len = 0;
 
-	if (n == 0)
+	while (copy_of_n > 0)
 	{
-		_putchar('0');
-		return;
+		len++;
+		copy_of_n >>= 1;
 	}
+	len -= 1;
 
-	bit_mask = 1UL << (sizeof(unsigned long int) * 8 - 1);
+	if (len > 0)
+		bit_mask = bit_mask << len;
 
-	while ((bit_mask & n) == 0)
+	while (bit_mask > 0)
 	{
+		if (n & bit_mask)
+			_putchar('1');
+		else
+			_putchar('0');
+
 		bit_mask >>= 1;
-		while (bit_mask != 0)
-		{
-			if (bit_mask & n)
-				_putchar('1');
-			else
-			{
-				_putchar('0');
-				bit_mask >>= 1;
-			}
-		}
 	}
 }
