baa8e6d072ac01afeb4bc1ef69c1f9fc2374c95c
Nick <kiprotichnickson20@gmail.com>
2023-04-15 22:13:51 +0300

    added condition in case text_content is empty

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -24,6 +24,12 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	}
 
+	if (!text_content)
+	{
+		close(file_descriptor);
+		return (1);
+	}
+
 	while (text_content[text_size] != '\0')
 	{
 		text_size++;
