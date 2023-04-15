65770bee34f60793921faec9249ac666bdc599c9
Nick <kiprotichnickson20@gmail.com>
2023-04-16 02:37:07 +0300

    removed bufer

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -8,7 +8,6 @@ int append_text_to_file(const char *filename, char *text_content)
 {
 
 	int fd;
-	char *buffer;
 	int text_size = 0;
 
 	if (!filename)
@@ -20,15 +19,15 @@ int append_text_to_file(const char *filename, char *text_content)
 		return (-1);
 	}
 
+	if (text_content ==NULL)
+	{
+		close(fd);
+		return (-1);
+	}
+
 	while (text_content[text_size] != '\0')
 	{
 		text_size++;
-	}
-
-	buffer = malloc(sizeof(char) * text_size);
-        if (buffer == NULL)
-	{
-                return (-1);
 	}
 
 	if (text_size == '0')
@@ -37,7 +36,7 @@ int append_text_to_file(const char *filename, char *text_content)
 		return (0);
 	}
 
-	write(fd, buffer, text_size);
+	write(fd, text_content, text_size);
 	close(fd);
 
 	return (1);
