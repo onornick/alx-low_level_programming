446ef23356b59abad3a8ba8fdae90eeb7ee1bf8b
Nick <kiprotichnickson20@gmail.com>
2023-04-16 02:57:20 +0300

    add O_CREAT flag to check if file exists

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -16,7 +16,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 
-	fd = open(filename, O_WRONLY | O_APPEND);
+	fd = open(filename, O_WRONLY | O_APPEND | O_CREAT, S_IRUSR | S_IWUSR);
 	if (fd == -1)
 	{
 		return (-1);
