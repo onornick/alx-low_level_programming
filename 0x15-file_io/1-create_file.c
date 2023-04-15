fda66eca7550ecf0746218156b3bd39b66c10f3e
Nick <kiprotichnickson20@gmail.com>
2023-04-15 22:29:16 +0300

    reverted to previous version

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -24,12 +24,6 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	}
 
-	if (!text_content)
-	{
-		close(file_descriptor);
-		return (1);
-	}
-
 	while (text_content[text_size] != '\0')
 	{
 		text_size++;
