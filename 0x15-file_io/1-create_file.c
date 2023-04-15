195c931faedd56de66cfac587255400dbb20c2a0
Nick <kiprotichnickson20@gmail.com>
2023-04-15 21:27:54 +0300

    changed the algorithm

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,8 +8,7 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	char *buffer;
-	ssize_t read_lines;
+	ssize_t byte_write;
 	int file_descriptor, text_size;
 
 	if (!filename)
@@ -19,32 +18,27 @@ int create_file(const char *filename, char *text_content)
 			O_WRONLY | O_CREAT | O_TRUNC,
 			S_IRUSR | S_IWUSR);
 
-	if (!file_descriptor)
+	if (file_descriptor == -1)
 	{
-		close(file_descriptor);
 		return (-1);
 	}
-	if (chmod(filename, S_IRUSR | S_IWUSR) == -1)
-		return (-1);
 
 	while (text_content[text_size] != '\0')
 	{
 		text_size++;
 	}
-	buffer = malloc((sizeof(char)) * (text_size));
-	if (buffer == NULL)
+
+	if (text_content)
 	{
-		close(file_descriptor);
-		return (0);
-	}
-	read_lines = read(file_descriptor, buffer, text_size);
-	if (read_lines == -1)
-	{
-		return (1);
+		byte_write = write(file_descriptor, text_content, text_size);
+		if(byte_write == -1)
+		{
+			close(file_descriptor);
+			return (-1);
+		}
 	}
 
-	write(STDOUT_FILENO, buffer, read_lines);
 	close(file_descriptor);
 
-	return (0);
+	return (1);
 }
