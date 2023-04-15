a8fad2425d2171dc4829b374d5b22ce257a3f63c
Nick <kiprotichnickson20@gmail.com>
2023-04-15 22:32:27 +0300

    betty style review

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -24,6 +24,12 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	}
 
+	if (!text_content)
+	{
+		close(fd);
+		return (1);
+	}
+
 	while (text_content[text_size] != '\0')
 	{
 		text_size++;
@@ -32,7 +38,7 @@ int create_file(const char *filename, char *text_content)
 	if (text_content)
 	{
 		byte_write = write(file_descriptor, text_content, text_size);
-		if(byte_write == -1)
+		if (byte_write == -1)
 		{
 			close(file_descriptor);
 			return (-1);
