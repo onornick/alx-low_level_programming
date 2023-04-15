a7f22dc7a8a0fed17ff9950bf9863c3a583869ef
Nick <kiprotichnickson20@gmail.com>
2023-04-15 04:43:36 +0300

    added chmod

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,6 +16,12 @@ int create_file(const char *filename, char *text_content)
 	file_descriptor = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (file_descriptor == -1)
 		return (-1);
+	if (chmod(filename, S_IRUSR | S_IWUSR) == -1)
+	{
+		fclose(file);
+		return (0);
+	}
+
 	if (!text_content)
 	{
 		close(file_descriptor);
