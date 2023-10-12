bbfec8396976dd73c20d31e3eba78721a0659aa4
Nickson Kiprotich <kiprotichnickson20@gmail.com>
2023-10-12 15:46:40 +0300

    initial commit

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include "search_algos.h"
+/**
+ * binary_search - function that searches for a value
+ * @array: the array to be parsed
+ * @size: size of the array
+ * @value: the data being searched
+ * Return: the index/position of the value
+ * Description: function that searches for a value
+ */
+
+int binary_search(int *array, size_t size, int value)
+{
+	size_t l = 0;
+	size_t x;
+	size_t r = size - 1;
+	size_t mid = (l + r) / 2;
+
+	while (l <= r)
+	{
+		printf("Searching in array: ");
+		for (x = l; x <= r; x++)
+		{
+			printf("%ld%s", x, x == r ? "\n" : ", ");
+		}
+		printf("\n");
+		if (array[mid] == value)
+		{
+			return (mid);
+		}
+		else if (array[mid] < value)
+		{
+			l = mid + 1;
+			mid = (l + r) / 2;
+		}
+		else if (array[mid] > value)
+		{
+			r = mid - 1;
+			mid = (l + r) / 2;
+		}
+	}
+
+	return (-1);
+
+}
